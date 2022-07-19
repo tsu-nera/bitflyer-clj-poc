4322f7d0211442cca19b73f7c14152be6276b6d9
tsu-nera <fox10225fox@gmail.com>
2022-07-19 15:35:52 +0900

    :construction:指値計算(手抜き)

--- src/bitflyer_clj/lib.clj
+++ src/bitflyer_clj/lib.clj
@@ -1,4 +1,6 @@
-(ns bitflyer-clj.lib)
+(ns bitflyer-clj.lib
+  (:require
+   [bitflyer-clj.api :as api]))
 
 (defn get-asset
   "JPY残高を参照する関数"
@@ -8,9 +10,16 @@
   "JPY証拠金を参照する関数"
   [])
 
-(defn get-effective-tick
-  "板情報から実効Ask/Bid(=指値を入れる基準値)を計算する関数"
-  [])
+#_(defn get-eff-ticker
+    "板情報から実効Ask/Bid(=指値を入れる基準値)を計算する関数"
+    [])
+
+(def th-size 0.01)
+
+(defn ->limit-price
+  "TODO 板情報から実効bid/askを計算"
+  [board]
+  (-> board first :price))
 
 (defn market
   "成行注文"
@@ -27,3 +36,17 @@
 (defn get-status
   "注文ステータス取得"
   [id])
+
+(comment
+
+  (def order-book (api/fetch-order-book))
+
+  (def bids (:bids order-book))
+  (count bids)
+  (->limit-price bids)
+
+  (def asks (:asks order-book))
+  (count asks)
+  (->limit-price bids)
+
+  )
